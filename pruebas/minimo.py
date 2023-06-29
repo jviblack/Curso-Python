ee64009c05fee26c2374f1f5c862367177dcbe12
jviblack <javie.couto.dominguez@gmail.com>
2023-06-29 20:20:29 +0200

    Confirmado

---
+++ pruebas/minimo.py
@@ -0,0 +1,10 @@
+# Elegir el mínimo
+a = int(input("Ingrese un valor A: "))
+b = int(input("Ingrese un valor B: "))
+
+def menor(a, b):
+
+    if a <= b:
+        print("El numero menor es: ", a)
+    else:
+        print("El numero menor es: ", b)
