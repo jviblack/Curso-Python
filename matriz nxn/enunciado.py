6ba4398c98b855a1499172e2cdba4b54e681a9d0
jviblack <javie.couto.dominguez@gmail.com>
2023-06-29 18:52:19 +0200

    Confirmado

---
+++ matriz nxn/enunciado.py
@@ -0,0 +1,30 @@
+"""
+
+Escribir un programa en Python que genere una matriz de tamaño NxN y la llene con números aleatorios entre 0 y 9. 
+
+El programa deberá imprimir la matriz generada y luego calcular la suma de los elementos de cada fila y columna. 
+
+Finalmente, deberá imprimir la suma de cada fila y columna.
+
+
+El programa deberá incluir las siguientes características:
+
+Generación de la matriz: 
+        El programa deberá generar una matriz cuadrada de tamaño NxN, donde N es un número entero ingresado por el usuario.
+
+Rellenar la matriz con números aleatorios: 
+        El programa deberá rellenar la matriz con números aleatorios entre 0 y 9.
+
+Imprimir la matriz: 
+        El programa deberá imprimir la matriz generada en pantalla.
+
+Calcular la suma de cada fila y columna: 
+        El programa deberá calcular la suma de los elementos de cada fila y columna y almacenarlas en dos listas.
+        
+Imprimir la suma de cada fila y columna: 
+        El programa deberá imprimir en pantalla la suma de cada fila y columna.
+
+Además, se sugiere que el programa incluya manejo de excepciones en caso de que el usuario ingrese un valor no válido para N, 
+también que incluya comentarios para explicar el código y finalmente que se hagan los test unitarios necesarios para asegurar que el resultado es el esperado.
+
+"""
